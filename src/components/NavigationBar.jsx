32121c4bf07a4ab8d303604b8bc1f01828282f37
sachithrakalani <kalanisathyangi@gmail.com>
2025-01-27 13:18:54 +0530

    Style Navigation bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,14 +1,19 @@
+import style from "./navigationbar.module.css";
+
 export default function NavigationBar() {
   return (
-    <div>
-      <header>
-        <div>
-          <h3>VIVINO</h3>
-          <input placeholder="Search..."></input>
-          <h3>sign In</h3>
-          <h3>Cart</h3>
+    <div style={{ position:"fixed",width:"100%"}}>
+      <header className={style.navbar}>
+        <div className={style.navbarfirst}>
+          <h3 className={style.title}>VIVINO</h3>
+          <input
+            className={style.searchbar}
+            placeholder="Search for Wines..."
+          />
+          <h3 className={style.links}>sign In </h3>
+          <h3 className={style.links}>Cart</h3>
         </div>
-        <div>
+        <div className={style.navbarsecond}>
           <h3>Wines</h3>
           <h3>Offers</h3>
           <h3>Grapes</h3>
