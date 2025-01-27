76c8de3443bae4c420a94fa20ab62bd1644ddba6
sachithrakalani <kalanisathyangi@gmail.com>
2025-01-27 14:07:28 +0530

    Added font awesome icons and style

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,16 +2,21 @@ import style from "./navigationbar.module.css";
 
 export default function NavigationBar() {
   return (
-    <div style={{ position:"fixed",width:"100%"}}>
+    <div style={{ position: "fixed", width: "100%" }}>
       <header className={style.navbar}>
         <div className={style.navbarfirst}>
           <h3 className={style.title}>VIVINO</h3>
-          <input
-            className={style.searchbar}
-            placeholder="Search for Wines..."
-          />
-          <h3 className={style.links}>sign In </h3>
-          <h3 className={style.links}>Cart</h3>
+          <div className={style.search}>
+            <i className={`fas fa-search ${style.searchIcon}`}></i>
+            <input
+              className={style.searchbar}
+              placeholder="Search for Wines..."
+            />
+          </div>
+          <div className={style.links}>
+          <i className="fas fa-user" style={{ marginRight: "25px" }}></i>
+            <i className={`fas fa-shopping-cart`}></i>
+          </div>
         </div>
         <div className={style.navbarsecond}>
           <h3>Wines</h3>
